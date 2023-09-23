5f8b5652d3a24a7d90b4faf37396fcf334bf30e8
asSqr <ryo.yossy@live.jp>
2023-09-23 17:21:40 +0900

    微修正

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -85,12 +85,12 @@ export default function ListLayout({
           </h1>
           <div className="relative max-w-lg">
             <label>
-              <span className="sr-only">Search articles</span>
+              <span className="sr-only">記事を探す</span>
               <input
-                aria-label="Search articles"
+                aria-label="記事を探す"
                 type="text"
                 onChange={(e) => setSearchValue(e.target.value)}
-                placeholder="Search articles"
+                placeholder="記事を探す"
                 className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
               />
             </label>
