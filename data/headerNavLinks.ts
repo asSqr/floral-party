40573c564925343b60ec7860f04f897d573969bc
asSqr <ryo.yossy@live.jp>
2023-09-30 22:19:04 +0900

    [#4] プロジェクトのタブを削除

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'ホーム' },
   { href: '/blog', title: 'ブログ' },
   { href: '/tags', title: 'タグ' },
-  { href: '/projects', title: 'プロジェクト' },
   { href: '/about', title: 'ページ作者について' },
 ]
 
