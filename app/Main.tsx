31d96ee87965ec5318ab6a24091c9db66b2fa331
asSqr <ryo.yossy@live.jp>
2023-12-09 22:52:46 +0900

    fix: メインページに Introduction を追加

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,7 +2,7 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
+import Image from '@/components/Image'
 
 const MAX_DISPLAY = 5
 
@@ -11,81 +11,388 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            最新の投稿
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">「花を見る会」について</h1>
+
+          <p className="pt-6 pb-6 text-lg leading-7 text-gray-900 dark:text-gray-100">
+            「花を見る会」は、「花見という身近なテーマで色々な人と交流したい！」という願いの下、2023 年 5 月に発足しました。<br />
+            それからほぼ毎月開催しています。X (旧 Twitter) で募集したたくさんの方に参加いただきました。
+
+            当会では有名な名所ではなく、できるだけ空いている穴場のスポットで花を見ることをコンセプトとしています。<br />
+            当会の植物アドバイザーの持つ豊富な穴場リストを参考に開催場所を選定しています。
           </p>
-        </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && '投稿が見つかりませんでした'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
+
+          <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              最新の投稿
+            </h1>
+          </div>
+          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+            {!posts.length && '投稿が見つかりませんでした'}
+            {posts.slice(0, MAX_DISPLAY).map((post) => {
+              const { slug, date, title, summary, tags } = post
+              return (
+                <li key={slug} className="py-12">
+                  <article>
+                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                        </dd>
+                      </dl>
+                      <div className="space-y-5 xl:col-span-3">
+                        <div className="space-y-6">
+                          <div>
+                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                              <Link
+                                href={`/blog/${slug}`}
+                                className="text-gray-900 dark:text-gray-100"
+                              >
+                                {title}
+                              </Link>
+                            </h2>
+                            <div className="flex flex-wrap">
+                              {tags.map((tag) => (
+                                <Tag key={tag} text={tag} />
+                              ))}
+                            </div>
+                          </div>
+                          <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                            {summary}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
+                        <div className="text-base font-medium leading-6">
+                          <Link
+                            href={`/blog/${slug}`}
+                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                            aria-label={`"${title}" を読みましょう`}
+                          >
+                            もっと見る &rarr;
+                          </Link>
                         </div>
                       </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`"${title}" を読みましょう`}
-                        >
-                          もっと見る &rarr;
-                        </Link>
-                      </div>
                     </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
+                  </article>
+                </li>
+              )
+            })}
+          </ul>
+
+          {posts.length > MAX_DISPLAY && (
+            <div className="flex justify-end text-base font-medium leading-6">
+              <Link
+                href="/blog"
+                className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                aria-label="全ての投稿"
+              >
+                全ての投稿 &rarr;
+              </Link>
+            </div>
+          )}
+
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">過去の開催記録</h1>
+
+          <p className="pt-6 pb-6 text-lg leading-7 text-gray-900 dark:text-gray-100">
+            過去の開催記録については以下をご覧ください：
+          </p>
+
           <Link
-            href="/blog"
+            href='/blog/1st-rose'
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="全ての投稿"
           >
-            全ての投稿 &rarr;
+            第 1 回 バラ (@与野公園)
           </Link>
+
+          <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='バラ 1'
+                src='/static/images/rose/rose-6.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='バラ 2'
+                src='/static/images/rose/rose-8.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='バラ 3'
+                src='/static/images/rose/rose-12.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='バラ 4'
+                src='/static/images/rose/rose-10.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+          </div>
+
+          <Link
+            href='/blog/2nd-hydrangea'
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          >
+            第 2 回 アジサイ (@昭和記念公園)
+          </Link>
+
+          <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アジサイ 1'
+                src='/static/images/hydrangea/hydrangea-1.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アジサイ 2'
+                src='/static/images/hydrangea/hydrangea-3.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アジサイ 3'
+                src='/static/images/hydrangea/hydrangea-12.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アジサイ 4'
+                src='/static/images/hydrangea/hydrangea-15.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+          </div>
+
+          <Link
+            href='/blog/3rd-morning-glory'
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          >
+            第 3 回 アサガオ (@文京朝顔・ほおづき市)
+          </Link>
+
+          <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アサガオ 1'
+                src='/static/images/morning-glory/morning-glory-1.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アサガオ 2'
+                src='/static/images/morning-glory/morning-glory-10.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アサガオ 3'
+                src='/static/images/morning-glory/morning-glory-13.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='アサガオ 4'
+                src='/static/images/morning-glory/morning-glory-16.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+          </div>
+
+          <Link
+            href='/blog/4th-hurricane-lily'
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          >
+            第 4 回 ヒガンバナ (@小石川植物園)
+          </Link>
+
+          <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ヒガンバナ 1'
+                src='/static/images/hurricane-lily/hurricane-lily-10.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ヒガンバナ 2'
+                src='/static/images/hurricane-lily/hurricane-lily-11.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ヒガンバナ 3'
+                src='/static/images/hurricane-lily/hurricane-lily-30.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ヒガンバナ 4'
+                src='/static/images/hurricane-lily/hurricane-lily-36.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+          </div>
+
+          <div className="pt-8 pb-8">
+            <Link
+              href='/blog/5th-cosmos'
+              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            >
+              第 5 回 コスモス (@都市農業公園)
+            </Link>
+
+            <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+              <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+                <Image
+                  alt='コスモス 1'
+                  src='/static/images/cosmos/cosmos-5.jpg'
+                  width={640}
+                  height={1000}
+                />
+              </figure>
+              <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+                <Image
+                  alt='コスモス 2'
+                  src='/static/images/cosmos/cosmos-15.jpg'
+                  width={640}
+                  height={1000}
+                />
+              </div>
+              <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+                <Image
+                  alt='コスモス 3'
+                  src='/static/images/cosmos/cosmos-18.jpg'
+                  width={640}
+                  height={1000}
+                />
+              </div>
+              <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+                <Image
+                  alt='コスモス 4'
+                  src='/static/images/cosmos/cosmos-32.jpg'
+                  width={640}
+                  height={1000}
+                />
+              </div>
+            </div>
+          </div>
+
+          <Link
+            href='/blog/special-germany'
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          >
+            特別編 マリーゴールド (@東京ドイツ村)
+          </Link>
+
+          <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ドイツ 1'
+                src='/static/images/germany/germany-36.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ドイツ 2'
+                src='/static/images/germany/germany-37.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ドイツ 3'
+                src='/static/images/germany/germany-14.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='ドイツ 4'
+                src='/static/images/germany/germany-25.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+          </div>
+
+          <Link
+            href='/blog/6th-metasequoia'
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          >
+            第 6 回 メタセコイア (@トーベ・ヤンソンあけぼの子どもの森公園)
+          </Link>
+
+          <div className="-mx-2 flex flex-wrap overflow-hidden xl:-mx-2">
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='メタセコイア 1'
+                src='/static/images/metasequoia/metasequoia-21.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+            <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='メタセコイア 2'
+                src='/static/images/metasequoia/metasequoia-29.jpg'
+                width={640}
+                height={1000}
+              />
+            </figure>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='メタセコイア 3'
+                src='/static/images/metasequoia/metasequoia-32.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+            <div className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">
+              <Image
+                alt='メタセコイア 4'
+                src='/static/images/metasequoia/metasequoia-36.jpg'
+                width={640}
+                height={1000}
+              />
+            </div>
+          </div>
         </div>
-      )}
-      {siteMetadata.newsletter?.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
-        </div>
-      )}
+      </div>
     </>
   )
 }
