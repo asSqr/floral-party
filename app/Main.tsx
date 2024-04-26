266200ac978cf9811c117b129e61ea6abeddab6d
asSqr <ryo.yossy@live.jp>
2024-04-26 23:23:10 +0900

    fix: 記事執筆完了

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import Image from '@/components/Image'
+import NextSchedules from '@/components/NextSchedules'
 
 const MAX_DISPLAY = 5
 
@@ -44,45 +45,7 @@ export default function Home({ posts }) {
             次回開催予定
           </h1>
 
-          <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
-            次回は{' '}
-            <span className="text-3xl text-primary-500 dark:hover:text-primary-400">4/28 (日)</span>{' '}
-            に{' '}
-            <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-              国営昭和記念公園
-            </span>{' '}
-            にて{' '}
-            <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-              ネモフィラ
-            </span>{' '}
-            を見る予定です！ <br /> <br /> <br />
-            次々回は{' '}
-            <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-              5/19 (日)
-            </span>{' '}
-            に{' '}
-            <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-              ポピー・ハッピースクエア
-            </span>{' '}
-            にて{' '}
-            <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ポピー</span>{' '}
-            を見る予定です！ <br /> <br /> <br />
-            申し込みは「花を見る会」{' '}
-            <Link
-              href={`mailto:${siteMetadata.email}`}
-              className="text-3xl text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            >
-              公式メール
-            </Link>{' '}
-            あるいは{' '}
-            <Link
-              href="https://twitter.com/floral_party"
-              className="text-3xl text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            >
-              公式 X
-            </Link>{' '}
-            へお願いします！
-          </p>
+          <NextSchedules />
 
           <div className="space-y-2 pb-8 pt-6 md:space-y-5">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
