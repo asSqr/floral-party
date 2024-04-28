74bb18c891a03c6278aeeefe74f1d9aab01038d2
asSqr <ryo.yossy@live.jp>
2024-04-28 20:07:03 +0900

    fix: 次回開催予定を修正

--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -4,11 +4,6 @@ const NextSchedules = () => {
   return (
     <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
       次回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">4/28 (日)</span> に{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">都立舎人公園</span>{' '}
-      にて <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ネモフィラ</span>{' '}
-      を見る予定です！ <br /> <br /> <br />
-      次々回は{' '}
       <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
         5/19 (日)
       </span> に{' '}
