7228534f424aab38d61772b525d96be72ae763cb
as_sqr <ryo.yossy@live.jp>
2024-05-20 19:20:31 +0900

    第 12 回ポピーの記事を追加 (#42)
    * feat: 第 12 回ポピーの記事を追加
    
    * fix: html タグのミスを修正
    
    * fix: トップページに第 12 回の記載を追加
    
    * fix: 次回開催記録を追加

--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -2,15 +2,13 @@ const NextSchedules = () => {
   return (
     <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
       次回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">5/19 (日)</span> に{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-        ポピー・ハッピースクエア
-      </span>{' '}
-      にて <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ポピー</span>{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">6/23 (日)</span> に{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">日比谷公園</span> にて{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ユリ</span>{' '}
       を見る予定です！ <br /> <br />
       イベント詳細ページは{' '}
       <a
-        href="/blog/events/12th-poppy"
+        href="/blog/events/13th-lily"
         className="text-3xl text-primary-400 dark:hover:text-primary-300"
       >
         こちら
