6f992c2d3e01778225e1784568c6e4f4acc6b8e2
asSqr <ryo.yossy@live.jp>
2024-06-24 01:28:53 +0900

    fix: 次々回の開催予定を追加

--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -16,7 +16,30 @@ const NextSchedules = () => {
         こちら
       </a>
       <br /> <br />
+      次々回は{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
+        8/11 (日)
+      </span> に{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">昭和記念公園</span>{' '}
+      にて <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ヒマワリ</span>{' '}
+      を見る予定です！ <br /> <br />
+      イベント詳細ページは{' '}
+      <a
+        href="/blog/events/15th-sunflower"
+        className="text-3xl text-primary-400 dark:hover:text-primary-300"
+      >
+        こちら
+      </a>
+      <br /> <br />
       申し込みは「花を見る会」{' '}
+      <a
+        href="https://peatix.com/group/16297931/events"
+        className="text-3xl text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      >
+        公式 Peatix
+      </a>{' '}
+      へお願いします！ <br /> <br />
+      何かご不明な点がありましたら、
       <a
         href={'mailto:floral.party2023@gmail.com'}
         className="text-3xl text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
