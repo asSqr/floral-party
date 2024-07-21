20df944425113682dc045c8639b319e2ab1b7bb7
asSqr <ryo.yossy@live.jp>
2024-07-21 16:48:37 +0900

    fix: 次回開催予定を修正

--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -2,24 +2,7 @@ const NextSchedules = () => {
   return (
     <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
       次回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">7/21 (日)</span> に{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">水元公園</span> にて{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-        ハス・オニバス・アサザ
-      </span>{' '}
-      を見る予定です！ <br /> <br />
-      イベント詳細ページは{' '}
-      <a
-        href="/blog/events/14th-lotus-waterlily"
-        className="text-3xl text-primary-400 dark:hover:text-primary-300"
-      >
-        こちら
-      </a>
-      <br /> <br />
-      次々回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-        8/11 (日)
-      </span> に{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">8/11 (日)</span> に{' '}
       <span className="text-3xl text-primary-500 dark:hover:text-primary-400">昭和記念公園</span>{' '}
       にて <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ヒマワリ</span>{' '}
       を見る予定です！ <br /> <br />
