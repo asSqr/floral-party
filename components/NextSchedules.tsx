c7f90d1f539e4e5d5fb142d150431f66a0035a45
asSqr <ryo.yossy@live.jp>
2024-11-25 21:49:23 +0900

    fix: 次回開催記録を修正

--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -2,13 +2,6 @@ const NextSchedules = () => {
   return (
     <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
       次回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">11/22 (金)</span> に{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">伊豆大島</span> にて{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">ツバキ</span>{' '}
-      を見る予定です！ <br />
-      (実質特別編で、参加できる方はあまりいないと思っています)
-      <br /> <br />
-      次々回は{' '}
       <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
         12/1 (日) or 12/7 (土)
       </span>{' '}
