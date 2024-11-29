468967e19cfda5761240a56e1fbefba60488ba87
asSqr <ryo.yossy@live.jp>
2024-11-29 12:37:33 +0900

    fix: 次回開催記録を修正

--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -2,14 +2,17 @@ const NextSchedules = () => {
   return (
     <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
       次回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-        12/1 (日) or 12/7 (土)
-      </span>{' '}
-      に <span className="text-3xl text-primary-500 dark:hover:text-primary-400">大田黒公園？</span>{' '}
-      にて <span className="text-3xl text-primary-500 dark:hover:text-primary-400">モミジ</span>{' '}
-      を見る予定です！
-      <br />
-      (紅葉の状況に応じて調整します) <br /> <br />
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">12/1 (日)</span> に{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">大田黒公園</span> にて{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">モミジ</span>{' '}
+      を見る予定です！ <br /> <br />
+      イベント詳細ページは{' '}
+      <a
+        href="https://peatix.com/event/4220122/view"
+        className="text-3xl text-primary-400 dark:hover:text-primary-300"
+      >
+        こちら
+      </a>
       <br /> <br />
       申し込みは「花を見る会」{' '}
       <a
