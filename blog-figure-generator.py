238b5cf887251e048ed52a7e707206cc2ebc0a8a
as_sqr <ryo.yossy@live.jp>
2025-01-31 16:49:19 +0900

    第 20 回 ボタン・アイスチューリップの記事を追加 (#54)
    * feat: 第 20 回の開催記事を執筆
    
    * fix: 次回開催予定を修正
    
    * fix: 記事本文を修正
    
    * fix: トップページに画像を追加
    
    * fix: タグを追加
    
    * fix: 写真が多すぎたので表示する写真を減らす
    
    * fix: 次回開催予定の日程変更を反映
    
    * fix: 次回開催予定に Peatix ページを追加
    
    * fix: 写真を大幅に削減

--- blog-figure-generator.py
+++ blog-figure-generator.py
@@ -1,4 +1,4 @@
-flower_name = 'camellia'
+flower_name = 'peony-ice-tulip'
 
-for i in range(433, 436):
+for i in range(261, 264):
     print(f'  <figure className="my-1 w-full overflow-hidden px-2 xl:my-1 xl:w-1/2 xl:px-2">\n    ![ツバキ {i}](/static/images/{flower_name}/{flower_name}-{i}.jpg)\n  </figure>')
--- components/NextSchedules.tsx
+++ components/NextSchedules.tsx
@@ -2,20 +2,18 @@ const NextSchedules = () => {
   return (
     <p className="pt-10 pb-10 text-lg leading-7 text-gray-900 dark:text-gray-100">
       次回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">1/26 (日)</span> に{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">上野公園</span> にて{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-        ボタン・アイスチューリップ
-      </span>{' '}
-      を見る予定です！ <br /> <br />
-      次々回は{' '}
-      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">
-        2/9 (日)
-      </span> に{' '}
+      <span className="text-3xl text-primary-500 dark:hover:text-primary-400">2/16 (日)</span> に{' '}
       <span className="text-3xl text-primary-500 dark:hover:text-primary-400">昭和記念公園</span>{' '}
       にて{' '}
       <span className="text-3xl text-primary-500 dark:hover:text-primary-400">フクジュソウ</span>{' '}
       を見る予定です！ <br /> <br />
+      イベント詳細ページは{' '}
+      <a
+        href="https://21th-amur-adonis.peatix.com"
+        className="text-3xl text-primary-400 dark:hover:text-primary-300"
+      >
+        こちら
+      </a>
       <br /> <br />
       申し込みは「花を見る会」{' '}
       <a
